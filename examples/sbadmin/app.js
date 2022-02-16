e696fa3647adb1619a1942dfbf1039c16ed741f9
robtweed <rtweed@mgateway.com>
2022-02-16 19:33:56 +0000

    Cleaned up app.js in example

--- examples/sbadmin/app.js
+++ examples/sbadmin/app.js
@@ -1,26 +1,13 @@
-//async function run() {
+const {golgi} = await import('../../src/golgi.js');
 
-  const {golgi} = await import('../../src/golgi.js');
+let context = {
+  componentPaths: {
+    sbadmin: '../examples/sbadmin/components/sbadmin/',
+    chart: '../examples/sbadmin/components/chart/'
+  },
+  assemblyPath: '../examples/sbadmin/assemblies/'
+};
 
+golgi.setLog(true);
 
-  let context = {
-    componentPaths: {
-      sbadmin: '../examples/sbadmin/components/sbadmin/',
-      chart: '../examples/sbadmin/components/chart/'
-    },
-    assemblyPath: '../examples/sbadmin/assemblies/'
-  };
-
-  golgi.setLog(true);
-
-  await golgi.renderAssembly('root_assembly', 'body', context);
-
-//}
-
-//run();
-
-
-
-
-
-
+await golgi.renderAssembly('root_assembly', 'body', context);
