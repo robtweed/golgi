96f1bd17313c15514f1a4fe31b376e20929ca75a
robtweed <rtweed@mgateway.com>
2022-02-25 16:24:19 +0000

    fixed path in homepage in new example

--- examples/conduit/assemblies/home_page.js
+++ examples/conduit/assemblies/home_page.js
@@ -3,7 +3,7 @@ export function load(ctx) {
   const gx=`
 <conduit-content-page name="home_page">
   <conduit-home-page>
-    <script src="/golgi-conduit/js/showdown/showdown.min.js" await="true" />
+    <script src="../conduit/js/showdown/showdown.min.js" await="true" />
   </conduit-home-page>
 </conduit-content-page>
   `;
