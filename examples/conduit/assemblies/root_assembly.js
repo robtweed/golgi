d04271ee33d9a18289046d4d1ca7bbece33c92e6
robtweed <rtweed@mgateway.com>
2022-02-25 16:19:36 +0000

    rootassembly path changes in new example

--- examples/conduit/assemblies/root_assembly.js
+++ examples/conduit/assemblies/root_assembly.js
@@ -2,12 +2,12 @@ export function load(ctx) {
 
   const gx=`
 <conduit-root golgi:hook="initialise" golgi:stateMap="user">
-  <script src="/golgi-conduit/js/auth0/jwt-decode.min.js" await="true"/> 
+  <script src="../conduit/js/auth0/jwt-decode.min.js" await="true"/> 
 
   <css src="//code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css" />
   <css src="//fonts.googleapis.com/css?family=Titillium+Web:700|Source+Serif+Pro:400,700|Merriweather+Sans:400,700|Source+Sans+Pro:400,300,600,700,300italic,400italic,600italic,700italic" />
   <css src="//demo.productionready.io/main.css" />
-  <css src="../css/styles.css" />
+  <css src="../conduit/css/styles.css" />
   <meta charset="utf-8" />
 </conduit-root>
   `;
